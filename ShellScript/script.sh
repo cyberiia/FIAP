2426fe3e091d343be8e0ef0c613ed86f275ed66e
Cyberia <87562989+cyberiia@users.noreply.github.com>
2022-09-02 10:21:43 -0300

    Create script.sh

---
+++ ShellScript/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Shell Script!
+# Exibe informações na tela:
+
+echo -e "Meu primeiro programa em Shell Script!"
+echo -e
+echo -e "Meu nick é $USER e minha máquina, $HOSTNAME."
+echo -e
+echo -e "Linux é melhor que Wuindows!"
